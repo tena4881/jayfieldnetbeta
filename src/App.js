9e6a662cc78e69dee14ac52a235c01378aa54576
Antenhe Tena <antenhetena@Antenhes-MacBook-Air.local>
2021-01-06 12:54:43 -0600

    fixed styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
-import SigninPage from './pages/signin';
+import Profile from './pages/profile';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/' component={Home} exact />
-        <Route path='/signin' component={SigninPage} exact />
+        <Route path='/profile' component={Profile} exact />
       </Switch>
     </Router>
   );
---
+++ src/pages/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProfilePg from '../components/SignIn';
+
+function Profile() {
+  
+  return (
+    <>
+      <ProfilePg />
+
+    </>
+  );
+}
+
+export default Profile;
