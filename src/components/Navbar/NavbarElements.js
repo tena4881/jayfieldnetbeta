9f6ca658eb5d1849b6dcc64e5c2a91dffe6293fd
Antenhe Tena <antenhetena@Antenhes-MacBook-Air.local>
2021-01-08 19:40:08 -0600

    fix

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -18,6 +18,7 @@ export const Nav = styled.nav`
 
   @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
+    background: ${({ scrollNav }) => (scrollNav ? 'transparent' : 'transparent')};
   }
 `;
 
