2526d83624c7d9808690e7b61f55260fda0aa429
Antenhe Tena <antenhetena@Antenhes-MacBook-Air.local>
2021-03-04 14:22:55 -0500

    Updates Vault Smart contract

--- contracts/JayFieldVault.sol
+++ contracts/JayFieldVault.sol
@@ -1,28 +1,28 @@
 pragma solidity ^0.8.0;
 
+import "./JayFieldCoin.sol";
 
-contract ERC20 {
-    function totalSupply() public view returns (uint supply);
-    function balanceOf(address _owner) public view returns (uint balance);
-    function transfer(address _to, uint _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
-    function approve(address _spender, uint _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint remaining);
-    function decimals() public view returns(uint digits);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
 
 
 contract Test{
-
+    JayFieldCoin public token;
+    
+    event Contributed(uint amount);
+    event Sold(uint256 amount);
     address owner;
     address payable withdrawAddr;
-
+    uint rate;
+    
     mapping(address => uint) balances;
-
-    constructor(address payable _withdrawAddr) payable public {
+    mapping(address => uint) JFCAddress;
+    
+    
+    
+    constructor(address payable _withdrawAddr, JayFieldCoin _token) payable public {
         owner = msg.sender;
         withdrawAddr = _withdrawAddr;
+        token = _token;
+        rate = 10000;
     }
 
     modifier onlyOwner(){
@@ -30,32 +30,59 @@ contract Test{
         _;
     }
     
-    function approveDeposit(address tokenAddress) public {
-        ERC20 token = ERC20(tokenAddress);
-        token.approve(address(this), token.balanceOf(msg.sender));
+    
+    
+    //TO-DO Make Only owner
+   function addJFCToVault(uint _amount) public {
+            uint toJFC = _amount * 100;
+            require(this.getUserJFCBalance(msg.sender) > 0, "You dont have JFC to add!");
+            require(toJFC > 0, "You need to add at least some JFC");
+            uint256 allowance = getAllowance();
+            token.increaseAllowance(address(this), toJFC);
+            require(allowance >= toJFC, "Check the token allowance");
+            token.transferFrom(msg.sender, address(this), toJFC);
+            
+            
+    } 
+    
+    function getAllowance() public  view returns(uint){
+        return token.allowance(msg.sender, address(this));
     }
-
-    function depositToken(address tokenAddress) public {
-        ERC20 token = ERC20(tokenAddress);
-        uint tokenAmount = token.balanceOf(msg.sender);
-        token.transferFrom(msg.sender, address(this), tokenAmount);
-    }
-   
-    function Send() payable public {
+    
+    function jfcApprove(uint _amount) public returns(bool){
+        return token.approve(address(this), _amount);
+    }    
+       
+    //allows people to contribute to JayField Net   
+    //Turn on JFC vault balance checker before produciton!!!
+    function Contribute() payable public {
+        uint256 vaultBalance = this.getVaultJFCBalance();
+        //require(vaultBalance > 0,"Not enough JFC in the reserve" );
+        uint256 amountContributed = (msg.value)/(1 ether);
+        require(amountContributed > 0, "You need to contribute some Ether");
+        uint256 jftAmount = (amountContributed*10000)*100;
+        require(jftAmount <= vaultBalance, "Not enough JFC in the reserve");
+        token.transfer(msg.sender, jftAmount);
         balances[msg.sender] += msg.value;
     }
 
-    function getBalanceOwner() view public returns(uint){
-        return owner.balance;
+    
+    function getUserJFCBalance(address _user) view public returns(uint){
+        return token.balanceOf(_user);
     }
+
 
     
     function getBalanceWithdrawAddr() view public returns(uint){
         return withdrawAddr.balance;
     }
 
-    function getBalanceThis() view public returns(uint){
+    function getVaultETHBalance() view public returns(uint){
         return address(this).balance;
+    }
+    
+     function getVaultJFCBalance() view public returns(uint){
+        return token.balanceOf(address(this));
     }
     
     function getContribution() view public returns(uint){
