17c8573e26d93490a15d2dd4968230ff56f77160
Antenhe Tena <antenhetena@Antenhes-MacBook-Air.local>
2021-03-19 00:28:07 -0400

    face update

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -12,33 +12,14 @@ class AboutUs extends Component {
               <Col lg={5}>
                 <div className="features-box">
                   <h3>
-                  The decentralized collaborative business headquarters. 
-                  Where Social meets business. 
+                  The JayFieldNETwork is dedicated to creating expertise, enhancing business literacy and ownership within our culture.
+                  <br></br><br></br>All members are verified on blockchain technology and stay up to date on industry news.
                   </h3>
                   <p className="text-muted web-desc">
-                  JayField Consulting is tailored to creating expertise, enhancing business literacy and ownership within our culture. 
-                  JayField ensures their clients are complient and knowledgable on the first steps of any venture no matter the industry.
-                  </p>
-                  <ul className="text-muted list-unstyled mt-4 features-item-list">
-                    <li className="">Business Consultants</li>
-                    <li className="">
-                      Business Processing
-                    </li>
-                    <li className="">
-                      Business Plan Drafting
-                    </li>
-                    <li className="">Notary Public</li>
-                    <li className="">Video Courses</li>
-                    <li className="">Tax Preperation</li>
-                    <li className="">Financial Tips</li>
-                    <li className="">Solution Expert</li>
-                  </ul>
-                  <Link
-                    to="#"
-                    className="btn btn-primary mt-4 waves-effect waves-light"
-                  >
-                    Learn More <i className="mdi mdi-arrow-right"></i>
-                  </Link>
+                  
+                   </p>
+                  
+                 
                 </div>
               </Col>
               <Col lg={7}>
